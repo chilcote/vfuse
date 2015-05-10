73d794d3b950f0acaa46e1d468601850ec33befa
Joseph Chilcote <chilcote+github@gmail.com>
2015-05-10 08:47:04 -0700

    updated packer scripts forked from https://github.com/timsutton/osx-vm-templates

--- packer-scripts/chef-omnibus.sh
+++ packer-scripts/chef-omnibus.sh
@@ -4,4 +4,12 @@
 # https://github.com/timsutton/osx-vm-templates/blob/master/scripts/chef-omnibus.sh
 
 # http://www.opscode.com/chef/install
-curl -L https://www.opscode.com/chef/install.sh | bash
+INSTALL_ARGS=""
+
+if [ "${CHEF_VERSION}" != "latest" ]; then
+    INSTALL_ARGS="-v ${CHEF_VERSION}"
+fi
+
+curl -LO https://www.opscode.com/chef/install.sh
+bash ./install.sh "${INSTALL_ARGS}"
+rm install.sh
