c47db80481ac45121f2cda2063914f589af55153
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-09 11:46:18 +0800

    Delete packer-scripts directory

--- packer-scripts/README.md
+++
@@ -1,4 +0,0 @@
-packer-scripts
-==============
-These scripts are forked from Tim Sutton's [osx-vm-templates](https://github.com/timsutton/osx-vm-templates/) repo.
-
--- packer-scripts/system-update.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-if [ "$UPDATE_SYSTEM" != "true" ] && [ "$UPDATE_SYSTEM" != "1" ]; then
-  exit
-fi
-
-echo "Downloading and installing system updates..."
-softwareupdate -i -a
